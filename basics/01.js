32700cf4923810c4462c1a2caecb3f1529f24948
asmitbiswas <asmitbiswas1308@gmail.com>
2025-09-21 02:45:23 +0530

    knowing about variables

---
+++ basics/01.js
@@ -0,0 +1,12 @@
+ const accountid = "123126";
+let accountname = "Asmit";
+var accountemail = "asmit@example.com";
+accountcity = "seoul";
+accountstate = "Texas"
+
+
+ accountname = "Amit";
+ accountemail = "asmit13241@gmail.com";
+ accountcity = "tokyo";
+
+console.table([accountid, accountname, accountemail, accountcity, accountstate]);
