4cdf8f9b1835e94383af4449615b1e7f5975f186
asmitbiswas <asmitbiswas1308@gmail.com>
2025-09-21 02:46:18 +0530

    datatypes

---
+++ basics/02.js
@@ -0,0 +1,29 @@
+"use strict";//uses the latest version of js
+// alert(3+3);//  it is not browser it is node 
+
+console.log(3+
+    3);// javascript reads code line by line so it is not good way to write code
+
+console.log("Asmit")
+
+let name = "Asmit";
+let age = "11";
+let isLoggedIn = false;
+
+// num = 2 to the power 53
+// bigint will be used for large number 
+// string = ""
+// boolean = true/false
+// null = standalone value {object it is [null]} (when it is given in undefined state it becomes empty)
+// undefined = variable without value
+// symbol = unique identifier
+// object = key value pair
+
+// console.log(typeof name);
+
+// typeof = it tells the datatype of variable
+// console.log(typeof null);
+// console.log(typeof undefined);// special datatype = undefined
+// console.log(typeof Boolean);// function = bolean is a function
+
+ console.log(typeof string);// string is not defined so it gives undefined
