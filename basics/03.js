1e91c1ec270f5893448ee4cc755b434b2325247a
asmitbiswas <asmitbiswas1308@gmail.com>
2025-09-22 01:27:19 +0530

    converrsation in datatypes

---
+++ basics/03.js
@@ -0,0 +1,35 @@
+// let run = "100/7465dwfr"
+let ru = null
+let run = undefined
+
+
+// console.log(typeof run);
+
+// let valueInNumber = Number(run)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" & 33 => 33
+// "33abc"  => NaN
+// null => 0
+// undefined => NaN
+// " " => 0
+// "Asmit" => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "Asmit" => true
+// " " => false
+// null => false
+// undefined => false
+// "false" => true
+
+let number = 123
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
