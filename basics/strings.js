737a05b1b1ae731352849467ed5343a956630b2a
asmitbiswas <asmitbiswas1308@gmail.com>
2025-09-25 15:30:13 +0530

    '-'

---
+++ basics/strings.js
@@ -0,0 +1,103 @@
+const name = "Asmit"
+const repos = 3
+
+// console.log( name + repo);
+
+console.log(`Good Morning. My name is ${name} and I have ${repos} repos`);
+
+const gameName = new String('gtasanandreas')
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log( gameName.length);  // while accessing length property it will check in prototype
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(7));
+
+console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0,7)
+
+console.log(new String);
+
+const anotherString = gameName.slice(0,7)
+console.log(anotherString);
+
+const anotherString2 = gameName.split('a')
+console.log(anotherString2);
+
+const newStringOne = "   Hello World    "
+
+console.log(newStringOne.trim());
+
+const url = "https://www.google.com/Asmit%20Kumar"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('google'));
+
+console.log(url.includes('yahoo'));
+
+// the whole output
+/*
+Good Morning. My name is Asmit and I have 3 repos
+g
+{}
+13
+GTASANANDREAS
+gtasanandreas
+n
+8
+[String: '']
+gtasana
+Hello World
+https://www.google.com/Asmit-Kumar
+true
+PS D:\Javascript\basics> node "d:\Javascript\basics\strings.js"
+Good Morning. My name is Asmit and I have 3 repos
+g
+{}
+13
+GTASANANDREAS
+gtasanandreas
+n
+8
+[String: '']
+gtasana
+Hello World
+https://www.google.com/Asmit-Kumar
+true
+false
+PS D:\Javascript\basics> node "d:\Javascript\basics\strings.js"
+Good Morning. My name is Asmit and I have 3 repos
+g
+{}
+13
+GTASANANDREAS
+gtasanandreas
+n
+8
+[String: '']
+gtasana
+[ 'gt', 's', 'n', 'ndre', 's' ]
+Hello World
+https://www.google.com/Asmit-Kumar
+true
+false
+*/
+
+
+
+
+
+
+
+
+
+
+
