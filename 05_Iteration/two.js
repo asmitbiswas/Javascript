c984faa5a18b4d196994837b409856653a40322b
asmitbiswas <asmitbiswas1308@gmail.com>
2025-10-14 12:02:57 +0530

    loops disscusion

---
+++ 05_Iteration/two.js
@@ -0,0 +1,22 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2
+// }
+
+let myArray = ['Senku', 'Chrome', "Ruri", "Gen-Asagiri", "Sweka", "Kohaku", "Taigo", "Yuzuri-Ha"]
+
+let arr = 0 
+    while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`)
+    arr = arr + 1
+    
+}
+
+
+// do-while loop
+let score = 10
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 1000 );
