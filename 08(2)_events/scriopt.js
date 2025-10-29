0979d6d2c1ae709e806b7fe27cd95e5fca8bd20d
asmitbiswas <asmitbiswas1308@gmail.com>
2025-10-29 15:58:44 +0530

    BG changing web

---
+++ 08(2)_events/scriopt.js
@@ -0,0 +1,28 @@
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function () {
+
+    intervalId = setInterval(changeBGColor, 0.1561 )
+   
+
+    function changeBGColor(){
+         document.body.style.backgroundColor = randomColor()
+    }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId)
+  intervalId = null
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
