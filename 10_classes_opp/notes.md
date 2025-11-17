3ade9905690b2b1db79a50682ea5c9f20e5f7f08
asmitbiswas <asmitbiswas1308@gmail.com>
2025-11-17 20:19:05 +0530

    oop

---
+++ 10_classes_opp/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+# OOP
+
+# Object
+- collection of properties and methods
+- toLowerCase
+
+# why use oop
+
+# Parts of OOP
+Object literal
+
+- constructor functions
+- prototypes
+- classes
+- Instances(new, this)
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
---
+++ 10_classes_opp/oop.js
@@ -0,0 +1,31 @@
+const user = {
+    username:"Asmit",
+    LoggedIn : 8,
+    SignedIn : true,
+
+    getUserDetails: function(){
+        // console.log("Got details from database")
+        // console.log(`Username : ${this.username}`)
+        console.log(this)
+    }
+}
+
+console.log(user.username)
+console.log(user.getUserDetails())
+
+function User(username, IsloggedIn, LoginCount){
+    this.username = username;
+    this.IsloggedIn = IsloggedIn;
+    this.LoginCount = LoginCount;
+
+    this.greetings = () => {
+        console.log(`Welcome ${this.username}`)
+    }
+    return this
+
+}
+
+const userOne = new User("Asmit", "True", "584")
+const userTwo = new User("Axmxtx_ 3", "False", "1556")
+console.log(userOne.constructor)
+// console.log(userTwo)
